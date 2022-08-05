6ff43648274ec606bda02c9a425e6e62052f8778
Victor Matias <fvictor1705@gmail.com>
2022-08-05 17:41:10 -0300

    Resolve requisito 2.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      result: [],
+    };
+  }
+
+  render() {
+    const { result } = this.state;
+
+    return (
+      <div>
+        <label htmlFor="search">
+          <input type="text" id="search" placeholder="Digite um produto" />
+        </label>
+        {
+          !result.length && (
+            <span
+              data-testid="home-initial-message"
+            >
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </span>
+          )
+        }
+
+      </div>
+    );
+  }
+}

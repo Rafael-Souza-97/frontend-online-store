f2baa9f3ab9caa47e61051680050782713d37a9d
Lucas Rodrigues <rodrigges@Lucass-iMac-Pro.local>
2022-08-08 18:24:37 -0300

    Resolve requisito 10.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { getProductToLocalStorage } from '../services/localStorage';
+import {
+  getProductToLocalStorage,
+  setProductToLocalStorage,
+} from '../services/localStorage';
 
 export default class Cart extends Component {
   constructor() {
     super();
     this.state = {
       cartProducts: [],
+      filteredProducts: [],
     };
   }
 
@@ -13,40 +17,105 @@ export default class Cart extends Component {
     const products = JSON.parse(getProductToLocalStorage());
     this.setState({
       cartProducts: products,
+    }, () => {
+      const { cartProducts } = this.state;
+      const reduceCartProducts = cartProducts.reduce((acc, curr) => {
+        const hasRepeat = acc.includes(curr.title);
+        if (!hasRepeat) {
+          const a = curr.title;
+          acc.push(a);
+          return acc;
+        } return acc;
+      }, []);
+      const filteredProducts = [];
+      reduceCartProducts.forEach((title) => {
+        filteredProducts.push(cartProducts.find((el) => el.title === title));
+      });
+      this.setState({ filteredProducts });
     });
+  }
+
+  componentDidUpdate = () => {
+    const { cartProducts } = this.state;
+    setProductToLocalStorage(JSON.stringify(cartProducts));
   }
 
   returnQuantEqualCartProducts = (param) => {
     const { cartProducts } = this.state;
     const quantity = cartProducts.filter((product) => (
-      product.title === param
+      product.id === param
     ));
     return quantity.length;
   }
 
-  render() {
+  handleQuantity = ({ target }) => {
     const { cartProducts } = this.state;
+    const { name, id } = target;
+    if (name === 'remove') {
+      const item = document.querySelector('#item');
+      item.remove();
+      // solução temporária para requisito 10 (NÃO DEVERIA SER EXIGIDO, segundo README)
+    }
+    if (name === 'decrease') {
+      cartProducts.splice(id, 1); // params: index que quer remover, e quantos quer remover
+      this.setState({ cartProducts });
+    } if (name === 'increase') {
+      const productToAdd = cartProducts.find((el) => el.id === id);
+      this.setState((prevState) => ({
+        cartProducts: [...prevState.cartProducts, productToAdd],
+      }));
+    }
+  }
+
+  render() {
+    const { filteredProducts } = this.state;
 
     return (
       <div>
         {
-          !cartProducts.length ? (
+          !filteredProducts.length ? (
             <span
               data-testid="shopping-cart-empty-message"
             >
               Seu carrinho está vazio.
             </span>
           ) : (
-            cartProducts.map((product, index) => (
-              <div key={ index }>
+            filteredProducts.map((product, index) => (
+              <div key={ index } id="item">
                 <img src={ product.thumbnail } alt={ product.title } />
                 <h3 data-testid="shopping-cart-product-name">{ product.title }</h3>
                 <p>{ product.price }</p>
+                <button
+                  type="button"
+                  name="decrease"
+                  id={ index }
+                  data-testid="product-decrease-quantity"
+                  onClick={ this.handleQuantity }
+                >
+                  -
+                </button>
                 <p
                   data-testid="shopping-cart-product-quantity"
                 >
-                  { this.returnQuantEqualCartProducts(product.title) }
+                  { this.returnQuantEqualCartProducts(product.id) }
                 </p>
+                <button
+                  type="button"
+                  name="increase"
+                  id={ product.id }
+                  data-testid="product-increase-quantity"
+                  onClick={ this.handleQuantity }
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  name="remove"
+                  data-testid="remove-product"
+                  onClick={ this.handleQuantity }
+                >
+                  Remover
+                </button>
               </div>
             ))
           )
